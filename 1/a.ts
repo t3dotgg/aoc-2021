aab5e3de155770e54fd9378c0aa906f1d3599089
theo <me@t3.gg>
2021-11-30 21:06:31 -0800

    init and do day 1

---
+++ 1/a.ts
@@ -0,0 +1,15 @@
+const results = await Deno.readTextFile("./input.txt");
+
+const depths = results.split("\n").map((n) => parseInt(n, 10));
+
+console.log(depths);
+
+let increases = 0;
+let lastDepth: number | undefined = undefined;
+
+depths.forEach((depth) => {
+  if (lastDepth !== undefined && depth > lastDepth) increases++;
+
+  lastDepth = depth;
+});
+console.log("increases", increases);
---
+++ 1/b.ts
@@ -0,0 +1,13 @@
+const results = await Deno.readTextFile("./input.txt");
+
+const depths = results.split("\n").map((n) => parseInt(n, 10));
+
+console.log(depths);
+
+let increases = 0;
+
+for (let i = 0; i < depths.length - 3; i++) {
+  if (depths[i] < depths[i + 3]) increases++;
+}
+
+console.log("increases", increases);
