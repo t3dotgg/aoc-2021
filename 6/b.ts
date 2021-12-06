e44c682006fd8d6b59adc2fe8298a234e1a5f24e
theo <me@t3.gg>
2021-12-05 21:46:26 -0800

    Cleanup

--- 6/b.ts
+++ 6/b.ts
@@ -1,17 +1,14 @@
 export const aaaaa = "";
 
 const results = await Deno.readTextFile("./input.txt");
-
 const fish = results.split(",").map((f) => parseInt(f, 10));
 
-let days = 0;
-
 let counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
 fish.forEach((p) => {
   counts[p] = counts[p] + 1;
 });
 
+let days = 0;
 while (days < 256) {
   days++;
   const p = counts.shift();
@@ -20,7 +17,6 @@ while (days < 256) {
 }
 
 let sum = 0;
-
 counts.forEach((c) => (sum += c));
 
 console.log("done", sum);
