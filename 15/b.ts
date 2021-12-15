e70e4f85557ada62076618119d7b2b1eb0cdaa39
theo <me@t3.gg>
2021-12-15 00:11:46 -0800

    Cleanup b

--- 15/b.ts
+++ 15/b.ts
@@ -1,9 +1,6 @@
-export const aaaaa = "";
+export {};
 
-const results = await Deno.readTextFile("./input.txt");
-// const results = await Deno.readTextFile("./example.txt");
-
-const grid = results
+const grid = (await Deno.readTextFile("./input.txt"))
   .split("\n")
   .map((row) => row.split("").map((i) => parseInt(i, 10)));
 
@@ -12,18 +9,14 @@ const height = grid.length * 5;
 
 const fullGrid = Array.apply(null, Array(height)).map((_, y) =>
   Array.apply(null, Array(width)).map((_, x) => {
-    const realX = x % grid[0].length;
-    const realY = y % grid.length;
-
-    const score =
-      ((grid[realY][realX] +
+    return (
+      ((grid[y % grid.length][x % grid[0].length] +
         Math.floor(y / grid.length) +
         Math.floor(x / grid[0].length) -
         1) %
         9) +
-      1;
-
-    return score;
+      1
+    );
   })
 );
 
@@ -36,43 +29,15 @@ const getNeighbors = (x: number, y: number) => {
   ];
 };
 
-const scoreBoard: number[][] = Array.apply(null, Array(height)).map((_) =>
-  Array.apply(null, Array(width)).map((_) => Infinity)
-);
+const scoreBoard: number[][] = fullGrid.map((r) => r.map((_) => Infinity));
 
 scoreBoard[0][0] = 0;
-
-const traverse = (x: number, y: number) => {
-  if (fullGrid[y]?.[x] === undefined) {
-    return;
-  }
-
-  let score = Math.min(
-    ...getNeighbors(x, y).map(
-      (coords) => scoreBoard[coords[1]]?.[coords[0]] ?? Infinity
-    )
-  );
-
-  score = score + fullGrid[y][x];
-  if (x === fullGrid[0].length - 1 && y === fullGrid.length - 1) {
-    return;
-  }
-
-  if (score < scoreBoard[y][x]) {
-    scoreBoard[y][x] = score;
-
-    traverse(x, y + 1);
-    traverse(x + 1, y);
-  }
-};
-
-traverse(1, 0);
-traverse(0, 1);
 fullGrid[0][0] = 0;
 
-let lastBoard: number[][] = [];
-while (JSON.stringify(scoreBoard) !== JSON.stringify(lastBoard)) {
-  lastBoard = scoreBoard.map((r) => [...r]);
+// Iterate until no improvement occurs
+let prevBoard: number[][] = [];
+while (JSON.stringify(scoreBoard) !== JSON.stringify(prevBoard)) {
+  prevBoard = scoreBoard.map((r) => [...r]);
   for (let x = 0; x < fullGrid[0].length; x++) {
     for (let y = 0; y < fullGrid.length; y++) {
       if (x === 0 && y === 0) continue;
@@ -86,7 +51,4 @@ while (JSON.stringify(scoreBoard) !== JSON.stringify(lastBoard)) {
   }
 }
 
-console.log(
-  "done",
-  scoreBoard[scoreBoard.length - 1][scoreBoard[0].length - 1]
-);
+console.log(scoreBoard[height - 1][width - 1]);
