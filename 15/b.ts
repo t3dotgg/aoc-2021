d56adc2900ce54ac40fdde622f3aca0ab66105cb
theo <me@t3.gg>
2021-12-14 23:32:50 -0800

    It works fk

--- 15/b.ts
+++ 15/b.ts
@@ -27,15 +27,6 @@ const fullGrid = Array.apply(null, Array(height)).map((_, y) =>
   })
 );
 
-const scoreBoard: number[][] = Array.apply(null, Array(height)).map((_) =>
-  Array.apply(null, Array(width)).map((_) => Infinity)
-);
-
-const x = width - 1;
-const y = height - 1;
-
-scoreBoard[y][x] = fullGrid[y][x];
-
 const getNeighbors = (x: number, y: number) => {
   return [
     [x + 1, y],
@@ -45,7 +36,11 @@ const getNeighbors = (x: number, y: number) => {
   ];
 };
 
-const finalScores: number[] = [];
+const scoreBoard: number[][] = Array.apply(null, Array(height)).map((_) =>
+  Array.apply(null, Array(width)).map((_) => Infinity)
+);
+
+scoreBoard[0][0] = 0;
 
 const traverse = (x: number, y: number) => {
   if (fullGrid[y]?.[x] === undefined) {
@@ -58,24 +53,40 @@ const traverse = (x: number, y: number) => {
     )
   );
 
-  if (x === 0 && y === 0) {
-    finalScores.push(score);
-    console.log(score);
+  score = score + fullGrid[y][x];
+  if (x === fullGrid[0].length - 1 && y === fullGrid.length - 1) {
     return;
   }
-
-  score = score + fullGrid[y][x];
 
   if (score < scoreBoard[y][x]) {
     scoreBoard[y][x] = score;
 
-    traverse(x - 1, y);
-    traverse(x, y - 1);
+    traverse(x, y + 1);
+    traverse(x + 1, y);
   }
 };
 
-traverse(x - 1, y);
-traverse(x, y - 1);
+traverse(1, 0);
+traverse(0, 1);
+fullGrid[0][0] = 0;
 
-// Correct for test cases, off by between 1 and 3 for real answer 😅
-console.log("done", Math.min(...finalScores));
+let lastBoard: number[][] = [];
+while (JSON.stringify(scoreBoard) !== JSON.stringify(lastBoard)) {
+  lastBoard = scoreBoard.map((r) => [...r]);
+  for (let x = 0; x < fullGrid[0].length; x++) {
+    for (let y = 0; y < fullGrid.length; y++) {
+      if (x === 0 && y === 0) continue;
+      scoreBoard[y][x] =
+        Math.min(
+          ...getNeighbors(x, y).map(
+            (coords) => scoreBoard[coords[1]]?.[coords[0]] ?? Infinity
+          )
+        ) + fullGrid[y][x];
+    }
+  }
+}
+
+console.log(
+  "done",
+  scoreBoard[scoreBoard.length - 1][scoreBoard[0].length - 1]
+);
