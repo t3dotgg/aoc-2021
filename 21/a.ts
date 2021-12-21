679b48f3cfe64813cb4edbaf01b72a95050b376e
theo <me@t3.gg>
2021-12-20 22:02:53 -0800

    Dat 21

--- 21/a.ts
+++ 21/a.ts
@@ -1,5 +1,39 @@
 export {};
 
-const lines = (await Deno.readTextFile("./input.txt")).split("\n");
+// let p1 = 4;
+// let p2 = 8;
 
-console.log(lines);
+let p1 = 8;
+let p2 = 3;
+
+p1--;
+p2--;
+
+let dieRolls = 0;
+
+let p1s = 0;
+let p2s = 0;
+
+const getDieVal = () => {
+  const a = (dieRolls % 100) + 1;
+  dieRolls++;
+  const b = (dieRolls % 100) + 1;
+  dieRolls++;
+  const c = (dieRolls % 100) + 1;
+  dieRolls++;
+
+  return [a, b, c] as [number, number, number];
+};
+
+while (p1s < 1000 && p2s < 1000) {
+  p1 += getDieVal().reduce((s, c) => s + c, 0);
+  p1s += (p1 % 10) + 1;
+  if (p1s >= 1000) break;
+
+  p2 += getDieVal().reduce((s, c) => s + c, 0);
+  p2s += (p2 % 10) + 1;
+}
+
+const ans = Math.min(p1s, p2s) * dieRolls;
+
+console.log(ans);
--- 21/b.ts
+++ 21/b.ts
@@ -1,5 +1,58 @@
 export {};
 
-const lines = (await Deno.readTextFile("./input.txt")).split("\n");
+// let p1 = 4;
+// let p2 = 8;
 
-console.log(lines);
+let p1 = 8;
+let p2 = 3;
+
+p1--;
+p2--;
+
+const cache = new Map<string, [number, number]>();
+
+const doCheck = (
+  p1: number,
+  s1: number,
+  p2: number,
+  s2: number,
+  dofirst: boolean
+) => {
+  if (s1 >= 21) return [1, 0];
+  if (s2 >= 21) return [0, 1];
+
+  const marker = JSON.stringify({ p1, s1, p2, s2, dofirst });
+  if (cache.has(marker)) return cache.get(marker)!;
+
+  let p1wins = 0;
+  let p2wins = 0;
+
+  for (let x = 1; x < 4; x++) {
+    for (let y = 1; y < 4; y++) {
+      for (let z = 1; z < 4; z++) {
+        const score = x + y + z;
+        if (dofirst) {
+          const newP1 = (p1 + score) % 10;
+          const newS1 = s1 + newP1 + 1;
+          const [sp1, sp2] = doCheck(newP1, newS1, p2, s2, false);
+          p1wins += sp1;
+          p2wins += sp2;
+        } else {
+          const newP2 = (p2 + score) % 10;
+          const newS2 = s2 + newP2 + 1;
+          const [sp1, sp2] = doCheck(p1, s1, newP2, newS2, true);
+          p1wins += sp1;
+          p2wins += sp2;
+        }
+      }
+    }
+  }
+
+  cache.set(marker, [p1wins, p2wins]);
+
+  return [p1wins, p2wins];
+};
+
+const ans = doCheck(p1, p2, 0, 0, true);
+
+console.log(ans);
